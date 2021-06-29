d7e62c6777063776018ff0bf939175f07f5f6abb
afelix <austinjfelix@gmail.com>
2021-06-28 20:43:30 -0400

    saving progress

---
+++ components/HeaderItem.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface HeaderItemProps {
+  title: string;
+  Icon: any;
+}
+
+const HeaderItem: React.FC<HeaderItemProps> = ({ Icon, title }) => {
+  return (
+    <div className="flex flex-col items-center cursor-pointer group w-12 sm:w-20 hover:text-white">
+      <Icon className="h-8 mb-1 group-hover:animate-bounce" />
+      <h4 className="opacity-0 group-hover:opacity-100 tracking-widest">{title}</h4>
+    </div>
+  );
+};
+
+export default HeaderItem;
